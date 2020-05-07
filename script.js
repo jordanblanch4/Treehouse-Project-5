9d399fe7b3e58120907c1a1feea5c91994bf5288
Jordan Blanchard <jordanb17b@icloud.com>
2020-05-07 09:33:29 -0700

    update js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
+//FIXME:Image sizes
 //fetches data and parses it
 fetch('https://randomuser.me/api/?results=12&inc=name,picture,email,location')
     .then(response => response.json())
-    .then(data => gene(data.results)) //FIXEME: do something besides log data
+    .then(data => generateCard(data.results))
 
 
 //Search Markup
@@ -15,35 +16,38 @@ searchBar.innerHTML += `<form action="#" method="get">
 //gallery markup
 function generateCard(data) {
 const gallery = document.getElementById('gallery');
-
-let html = `<div class ="card">
-<div class="card-img-container">
-<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-</div>
-<div class="card-info-container">
-<h3 id="name" class="card-name cap">last first</h3>
-<p class="card-text">email</p>
-<p class="card-text cap">city, state</p>
-</div>
-</div>`
-
-
-html += `<div class="modal-container" hidden = true>
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
+const users = data.map(item => 
+    `<div class="card">
+    <div class="card-img-container">; 
+        <img class="card-img" src="${item.picture.large}" alt="profile picture"></img>
         </div>
-    </div>`;
-gallery.innerHTML += html;
-}
+        <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
+                        <p class="card-text">${item.email}</p>
+                        <p class="card-text cap">${item.location.city}</p>
+                    </div>
+                </div>
+<div class="modal-container">
+<div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+        <img class="modal-img" src="${item.picture.medium}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${item.name.first} ${item.name.last}</h3>
+        <p class="modal-text">${item.email}</p>
+        <p class="modal-text cap">${item.name.first}</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+    </div>
+</div>`
+)
+gallery.innerHTML += users;
+};
+
+
+
+
 
 //Global variables for the event listeners
 const modalDisplay = document.querySelector('.modal-container');
@@ -51,18 +55,18 @@ const card = document.querySelectorAll('.card')
 const modal = document.querySelector('.modal');
 const closeButton = document.getElementById('modal-close-btn');
 
-//FIXME
-// //open modal on card click
+// FIXME
+// open modal on card click
 // card.addEventListener('click', (e) => {
-//     modal.classList.remove('hidden') //FIXME not class list
-//modal.style.display = 'block';
-// })
+// /modal.classList.remove('hidden') //FIXME not class list
+// //modal.style.display = 'block';
+// // })
 
 
 
 //close modal on button click
 
-closeButton.addEventListener('click', (e) => {
-    modal.style.display = 'none';
-});
+// closeButton.addEventListener('click', (e) => {
+//     modal.style.display = 'none';
+// });
 
